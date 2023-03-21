37917484a40da9492e52a40d6cfe185e67c7737b
abrahamalex13 <alex.abraham13@gmail.com>
2023-03-21 12:17:06 -0500

    Extend tests

--- tests/test_empirical_pmf.py
+++ tests/test_empirical_pmf.py
@@ -18,10 +18,10 @@ def test_pool_rare():
 
 def test_thresh_no_pool_rare():
 
-    df = pd.DataFrame({"Make": ["Honda", "Honda", "Tesla", "Tesla"]})
+    x = pd.DataFrame({"Make": ["Honda", "Honda", "Tesla", "Tesla"]})
 
     epmf = empirical_pmf(
-        df["Make"], nobs_lt_pool_other=2, categories=[], include_other=False
+        x["Make"], nobs_lt_pool_other=2, categories=[], include_other=False
     )
     epmf["Make"] = epmf["Make"].astype("object")
 
@@ -29,3 +29,37 @@ def test_thresh_no_pool_rare():
 
     pd.testing.assert_frame_equal(epmf[["Make", "n"]], expected)
 
+
+def test_thresh_no_pool_rare_incl_other():
+
+    x = pd.DataFrame({"Make": ["Honda", "Honda", "Tesla", "Tesla"]})
+
+    epmf = empirical_pmf(
+        x["Make"], nobs_lt_pool_other=2, categories=[], include_other=True
+    )
+    epmf["Make"] = epmf["Make"].astype("object")
+
+    expected = pd.DataFrame(
+        {"Make": ["Honda", "Tesla", "OTHER"], "n": [2, 2, 0]}
+    )
+
+    pd.testing.assert_frame_equal(epmf[["Make", "n"]], expected)
+
+
+def test_predefined_categories():
+
+    x = pd.DataFrame({"Make": ["Honda", "Honda", "Tesla", "Tesla"]})
+
+    epmf = empirical_pmf(
+        x["Make"],
+        nobs_lt_pool_other=1,
+        categories=["Honda", "Ford"],
+        include_other=True,
+    )
+    epmf["Make"] = epmf["Make"].astype("object")
+
+    expected = pd.DataFrame(
+        {"Make": ["Honda", "OTHER", "Ford"], "n": [2, 2, 0]}
+    )
+
+    pd.testing.assert_frame_equal(epmf[["Make", "n"]], expected)
