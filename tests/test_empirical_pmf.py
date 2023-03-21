2c2983a714bd6e3edec9e55fbdc269dad6374e94
abrahamalex13 <alex.abraham13@gmail.com>
2023-03-21 12:35:58 -0500

    Extend tests

--- tests/test_empirical_pmf.py
+++ tests/test_empirical_pmf.py
@@ -46,7 +46,27 @@ def test_thresh_no_pool_rare_incl_other():
     pd.testing.assert_frame_equal(epmf[["Make", "n"]], expected)
 
 
-def test_predefined_categories():
+def test_predefined_categories_drop_novel_obs():
+    """
+    Demonstration of expected, though often not-preferred, behavior.
+    """
+
+    x = pd.DataFrame({"Make": ["Honda", "Honda", "Tesla", "Tesla"]})
+
+    epmf = empirical_pmf(
+        x["Make"],
+        nobs_lt_pool_other=2,
+        categories=["Honda", "Ford"],
+        include_other=False,
+    )
+    epmf["Make"] = epmf["Make"].astype("object")
+
+    expected = pd.DataFrame({"Make": ["Honda", "Ford"], "n": [2, 0]})
+
+    pd.testing.assert_frame_equal(epmf[["Make", "n"]], expected)
+
+
+def test_predefined_categories_include_other_novel_obs():
 
     x = pd.DataFrame({"Make": ["Honda", "Honda", "Tesla", "Tesla"]})
 
